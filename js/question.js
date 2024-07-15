a5079a60139012c390350fa46f643d28a5b018d8
6Flow <marcquin.agulhas@gmail.com>
2024-07-15 20:38:21 +0200

    Add files via upload

---
+++ js/question.js
@@ -0,0 +1,32 @@
+const quiz = [
+    {
+        q:'The SED demineralised water is stored in a ________  m 3  tank?',
+        options: ['A. 350','B. 500','C. 1600','D. 800 '],
+        answer: 1,
+        topic: "SED"
+    },
+    {
+        q:'The pH of the SED is ______.?',
+        options: ['A. 9.2','B. 8','C. 7','D. 6'],
+        answer: 2,
+        topic: "SED"
+    },
+    {
+        q:'SED 001 and 002PO are supplied by  380V essential boards?',
+        options: ['True','False'],
+        answer: 1,
+        topic: "SED"
+    },
+    {
+        q:'SER 001, 002 and 003PO are supplied by  380V essential boards?',
+        options: ['True','False'],
+        answer: 1,
+        topic: "SED"
+    },
+    {
+        q:'The fire water tanks are made up with _______ water?',
+        options: ['A. SER','B. SEC','C. SED','D. SEP '],
+        answer: 3,
+        topic: "SED"
+    },
+]
